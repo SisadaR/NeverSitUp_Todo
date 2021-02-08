93160a7a9ce5a1cba7373b957189a0cdf9d0237e
Sisada R <kong.sisada@gmail.com>
2021-02-08 22:26:05 +0700

    implement login connection with server

--- app/src/main/java/com/sisada/neversituptodo/view/LoginActivity.kt
+++ app/src/main/java/com/sisada/neversituptodo/view/LoginActivity.kt
@@ -1,12 +1,78 @@
 package com.sisada.neversituptodo.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
-import com.sisada.neversituptodo.R
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+
+import com.sisada.neversituptodo.api.Status
+import com.sisada.neversituptodo.databinding.ActivityLoginBinding
+import com.sisada.neversituptodo.etc.SharedInfo
+import com.sisada.neversituptodo.viewmodel.AuthViewModel
 
 class LoginActivity : AppCompatActivity() {
+
+    private var viewModel: AuthViewModel = AuthViewModel()
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+   
+        this.onLoginButtonClicked()
     }
+
+    private fun onLoginButtonClicked() {
+
+
+        binding.btnLogin.setOnClickListener {
+
+
+            val password = binding.tinPasswordValue.text.toString()
+            val email = binding.tinEmailValue.text.toString()
+            viewModel.login(email, password).observe(this, Observer {
+                it?.let { resource ->
+                    when (resource.status) {
+                        Status.SUCCESS -> {
+
+                            resource.data?.let { response ->
+                                SharedInfo.newToken(this, response.token)
+                                gotoTaskActivity()
+                            }
+
+                        }
+                        Status.ERROR -> {
+                            Log.e("LoginError", resource.message!!)
+
+                            AlertDialog.Builder(this)
+                                .setTitle("Something Wrong")
+                                .setMessage(resource.message)
+                                .setPositiveButton(android.R.string.yes, null)
+                                .setIcon(android.R.drawable.ic_dialog_alert)
+                                .show()
+                        }
+                        Status.LOADING -> {
+                            Log.d("Login", "Loading")
+                        }
+                    }
+                }
+            })
+
+
+        }
+    }
+
+    private fun gotoTaskActivity() {
+        Log.d("LOGIN_ACTICITY" , "go to task view")
+    }
+
+
 }
+
--- app/src/main/java/com/sisada/neversituptodo/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/sisada/neversituptodo/viewmodel/AuthViewModel.kt
@@ -3,7 +3,7 @@ package com.sisada.neversituptodo.viewmodel
 import androidx.lifecycle.ViewModel
 import com.sisada.neversituptodo.repository.Repository
 
-class LoginViewModel : ViewModel() {
+class AuthViewModel : ViewModel() {
 
     fun login(email:String,password:String) = Repository.login(email,password)
     fun register(name:String,email:String,password:String,age:Int) = Repository.register(name,email,password,age)
